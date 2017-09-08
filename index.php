7f7199f5c54c81ddb2f9e24be5158eb81120d727
Kaspars Andzans <kaspars_andzans@inbox.lv>
2017-09-08 13:14:06 +0000

    commit

--- index.php
+++ index.php
@@ -5,15 +5,29 @@ session_start();
 function __autoload($class_name) {
     if (file_exists('./classes/'.$class_name.'.php')) {
         require_once './classes/'.$class_name.'.php';
+        echo "required classes";
     } else if (file_exists('./controllers/'.$class_name.'.php')) {
         require_once './controllers/'.$class_name.'.php';
+        echo "required controllers";
     } else if (file_exists('./controllers/auth/'.$class_name.'.php')) {
         require_once './controllers/auth/'.$class_name.'.php';
+        echo "required controllers/auth";
+    } else {
+        echo "nothing required";
     }
 }
 
 echo "this is index page";
 
-require_once('./routes/Routes.php');
+if (file_exists('./routes/Routes.php')) {
+    require_once './routes/Routes.php';
+    echo "routes required";
+} else {
+    echo "routes not required";
+}
+
+//require_once('./routes/Routes.php');
+
+echo "and this is again index page";
 
 ?>
