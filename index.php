dd45fd3ae3fb9b40c1cfe35b8b85d2b29d9e41aa
Kaspars Andzans <kaspars_andzans@inbox.lv>
2017-09-08 14:08:25 +0000

    commit

--- index.php
+++ index.php
@@ -2,28 +2,37 @@
 
 session_start();
 
+/*Worked on Cloud9, but couldn't get it to work on Heroku and it doesn't seem so important right now
 function __autoload($class_name) {
-    echo "This is class name - ".$class_name." <br />"
     if (file_exists('./classes/'.$class_name.'.php')) {
         require_once './classes/'.$class_name.'.php';
-        echo "required classes <br />";
-    }
-    
-    if (file_exists('./controllers/'.$class_name.'.php')) {
+    } else if (file_exists('./controllers/'.$class_name.'.php')) {
         require_once './controllers/'.$class_name.'.php';
-        echo "required controllers <br />";
-    }
-    
-    if (file_exists('./controllers/auth/'.$class_name.'.php')) {
+    } else if (file_exists('./controllers/auth/'.$class_name.'.php')) {
         require_once './controllers/auth/'.$class_name.'.php';
-        echo "required controllers/auth <br />";
     }
 }
+*/
 
-echo "this is index page <br />";
+//classes
+require_once('./classes/Database.php');
+require_once('./classes/Route.php');
+
+//controllers
+require_once('./controllers/Controller.php');
+require_once('./controllers/Index.php');
+require_once('./controllers/Login.php');
+require_once('./controllers/Logout.php');
+require_once('./controllers/My_reviews.php');
+require_once('./controllers/Review.php');
+require_once('./controllers/Reviews.php');
+require_once('./controllers/User.php');
+
+//controllers/auth
+require_once('./controllers/auth/Facebook.php');
+require_once('./controllers/auth/Google.php');
+require_once('./controllers/auth/Twitter.php');
 
 require_once('./routes/Routes.php');
-
-echo "and this is again index page";
 
 ?>
