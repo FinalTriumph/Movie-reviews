22a932e31c76c18ced00cc765c4ae73546e567d9
Kaspars Andzans <kaspars_andzans@inbox.lv>
2017-09-08 15:49:27 +0000

    commit

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 session_start();
 
 function __autoload($class_name) {
-    echo "Trying this class - ".$class_name."<br />";
     if (file_exists('./classes/'.$class_name.'.php')) {
         require_once './classes/'.$class_name.'.php';
     } else if (file_exists('./controllers/'.$class_name.'.php')) {
@@ -11,7 +10,6 @@ function __autoload($class_name) {
     } else if (file_exists('./controllers/auth/'.$class_name.'.php')) {
         require_once './controllers/auth/'.$class_name.'.php';
     }
-    echo "Finished this class - ".$class_name."<br />";
 }
 
 require_once('./routes/Routes.php');
