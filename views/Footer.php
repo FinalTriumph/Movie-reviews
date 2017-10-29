97767bd6dc7be4abf6bedb5d79c7cc9d9649b87d
Kaspars Andzans <kaspars_andzans@inbox.lv>
2017-10-29 15:04:34 +0000

    commit

--- views/Footer.php
+++ views/Footer.php
@@ -2,7 +2,7 @@
     <img src="http://i.imgur.com/8ntI3rY.png" id="footer_arrow_img"/>
     <p1>Movie Reviews <span class='ft_sep'>|</span> PHP/MySQL practice project</p1>
     <hr id="footer_hr" />
-    <p1>2017 <span class='ft_sep'>|</span> <a href="http://finaltriumph.tk/" target="_blank">FinalTriumph</a></p1>
+    <p1>2017 <span class='ft_sep'>|</span> <a href="http://finaltriumph.eu/" target="_blank">FinalTriumph</a></p1>
 </div>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
 <script type="text/javascript">
